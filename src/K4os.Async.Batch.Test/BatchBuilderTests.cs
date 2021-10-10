d8d7c062eb94ac791a6c18dfda45ac34f1920e4c
Milosz Krajewski <miloszkrajewski@o2.pl>
2021-10-10 22:49:38 +0100

    more unit tests

--- src/K4os.Async.Batch.Test/BatchBuilderTests.cs
+++ src/K4os.Async.Batch.Test/BatchBuilderTests.cs
@@ -1,12 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.PlatformAbstractions;
 using Xunit;
 
 namespace K4os.Async.Batch.Test
 {
-	public class UnitTest1
+	public class BatchBuilderTests
 	{
 		[Fact]
 		public async Task AllRequestsAreMade()
@@ -82,6 +84,74 @@ namespace K4os.Async.Batch.Test
 
 			Assert.Equal(requests, responses);
 			Assert.True(batches <= (requests.Length + 99) / 100 + 1);
+		}
+
+		[Fact]
+		public async Task RequestIsMatchedWithResponse()
+		{
+			var builder = BatchBuilder.Create<int, int, string>(
+				r => r + 1000,
+				r => int.Parse(r) + 1000,
+				Requester);
+
+			async Task<string[]> Requester(int[] rl)
+			{
+				await Task.Delay(100);
+				return rl.Select(r => r.ToString()).ToArray();
+			}
+
+			var requests = Enumerable.Range(0, 1000).ToArray();
+			var tasks = requests.Select(r => builder.Request(r));
+			var responses = await Task.WhenAll(tasks);
+
+			Assert.Equal(requests.Select(r => r.ToString()), responses);
+		}
+
+		[Fact]
+		public async Task MissingResponseThrowException()
+		{
+			var builder = BatchBuilder.Create<int, int>(
+				r => r,
+				Requester);
+
+			async Task<int[]> Requester(int[] rl)
+			{
+				await Task.Delay(100);
+				return rl.Where(r => r != 337).ToArray();
+			}
+
+			var requests = Enumerable.Range(0, 1000).ToArray();
+			var tasks = requests.Select(r => builder.Request(r)).ToArray();
+
+			foreach (var r in requests)
+			{
+				if (r != 337)
+					Assert.Equal(r, await tasks[r]);
+				else
+					await Assert.ThrowsAsync<KeyNotFoundException>(() => tasks[r]);
+			}
+		}
+
+		[Fact]
+		public async Task WhenBatchFailsAllRequestFail()
+		{
+			var builder = BatchBuilder.Create<int, int>(
+				r => r,
+				Requester);
+
+			async Task<int[]> Requester(int[] rl)
+			{
+				await Task.Yield();
+				throw new ArgumentException("Not working!");
+			}
+
+			var requests = Enumerable.Range(0, 1000).ToArray();
+			var tasks = requests.Select(r => builder.Request(r)).ToArray();
+
+			foreach (var r in requests)
+			{
+				await Assert.ThrowsAsync<ArgumentException>(() => tasks[r]);
+			}
 		}
 	}
 }
