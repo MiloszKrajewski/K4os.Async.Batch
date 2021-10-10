2c610492f18ab23681990ac28e6b3f18d643ee8f
Milosz Krajewski <miloszkrajewski@o2.pl>
2021-10-10 22:50:05 +0100

    typo

--- src/K4os.Async.Batch.Test/BatchBuilderTests.cs
+++ src/K4os.Async.Batch.Test/BatchBuilderTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestPlatform.PlatformAbstractions;
 using Xunit;
 
 namespace K4os.Async.Batch.Test
